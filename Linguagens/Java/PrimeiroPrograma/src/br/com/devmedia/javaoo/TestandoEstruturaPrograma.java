e0d2f32bb757bcf5da2f3db80a1e61282b345aaa
igorjosebrilhante <igorjosebrilhante@gmail.com>
2014-05-30 02:00:29 +0000

    Arquivos com o Projeto PrimeiroPrograma.

---
+++ Linguagens/Java/PrimeiroPrograma/src/br/com/devmedia/javaoo/TestandoEstruturaPrograma.java
@@ -0,0 +1,23 @@
+package br.com.devmedia.javaoo;
+
+public class TestandoEstruturaPrograma {
+
+	/*
+	  Classe que apresenta a estrutura básica de um programa
+	*/
+	public static void main(String[] args) 
+	{
+		//entrada
+		int num1 = 10;
+		int num2 = 20;
+		
+		//processamento
+		int resultado = num1 + num2;
+		
+		//saída
+		System.out.println("Cheguei...");
+		System.out.println("Resultado: " + resultado);
+
+	}
+
+}
