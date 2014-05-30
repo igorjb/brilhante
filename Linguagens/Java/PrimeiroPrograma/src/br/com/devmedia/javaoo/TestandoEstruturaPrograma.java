ea1d977a35cc728c7a36422277cb45fcbc1aa52c
igorjosebrilhante <igorjosebrilhante@gmail.com>
2014-05-30 12:03:07 +0000

    Testei o programa com as 4 operacoes basicas.

--- Linguagens/Java/PrimeiroPrograma/src/br/com/devmedia/javaoo/TestandoEstruturaPrograma.java
+++ Linguagens/Java/PrimeiroPrograma/src/br/com/devmedia/javaoo/TestandoEstruturaPrograma.java
@@ -1,5 +1,7 @@
 package br.com.devmedia.javaoo;
 
+import java.util.Scanner;
+
 public class TestandoEstruturaPrograma {
 
 	/*
@@ -8,15 +10,31 @@ public class TestandoEstruturaPrograma {
 	public static void main(String[] args) 
 	{
 		//entrada
-		int num1 = 10;
-		int num2 = 20;
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro numero: ");
+		int num1 = entrada.nextInt();
+		
+		System.out.println("Digite o segundo numero: ");
+		int num2 = entrada.nextInt();
 		
 		//processamento
-		int resultado = num1 + num2;
+		int soma = num1 + num2;
+		
+		int subtr = num1 - num2;
+		
+		int multip = num1 * num2;
+		
+		int divis = num1 / num2;
+		
+		
 		
 		//saída
 		System.out.println("Cheguei...");
-		System.out.println("Resultado: " + resultado);
+		System.out.println("Soma: " + soma);
+		System.out.println("Subtracao: " + subtr);
+		System.out.println("Multiplicacao: " + multip);
+		System.out.println("Divisao: " + divis);
 
 	}
 
