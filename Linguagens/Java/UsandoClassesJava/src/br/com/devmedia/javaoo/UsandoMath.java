f6207a4f924b51e8489770be8026d8fc0b5b0fdd
igorjosebrilhante <igorjosebrilhante@gmail.com>
2014-06-14 15:18:41 +0000

    Usando Classe Math.

---
+++ Linguagens/Java/UsandoClassesJava/src/br/com/devmedia/javaoo/UsandoMath.java
@@ -0,0 +1,13 @@
+package br.com.devmedia.javaoo;
+
+public class UsandoMath 
+{
+	public static void main(String[] args) 
+	{
+		double result = Math.sqrt(81);
+		double aux = Math.pow(6, 2);
+		
+		System.out.println("Resultado: " + result);
+		System.out.println("Resultado: " + aux);
+	}
+}
