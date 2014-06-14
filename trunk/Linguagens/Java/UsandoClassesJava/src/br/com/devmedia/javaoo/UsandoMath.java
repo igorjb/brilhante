d4d838933fd93c706788aec3ac9294b491bc9347
igorjosebrilhante@gmail.com <igorjosebrilhante@gmail.com@9c8eae87-dc67-e4f6-7fff-6e8f81a4563f>
2014-06-14 17:32:31 +0000

    Exibe a data atual formatada e também por extenso. 2 métodos para cada uma. Cálculo da Hipotenusa, verifica qual o cateto maior e gera números aleatórios multiplicando-os por 10. Retorna a Data por extenso. Criado um método para isso;

--- trunk/Linguagens/Java/UsandoClassesJava/src/br/com/devmedia/javaoo/UsandoMath.java
+++ trunk/Linguagens/Java/UsandoClassesJava/src/br/com/devmedia/javaoo/UsandoMath.java
@@ -1,13 +1,34 @@
 package br.com.devmedia.javaoo;
 
+import javax.swing.JOptionPane;
+
 public class UsandoMath 
 {
 	public static void main(String[] args) 
 	{
-		double result = Math.sqrt(81);
-		double aux = Math.pow(6, 2);
+		//Digite o primeiro cateto
+		double cat1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o cat1:"));
 		
-		System.out.println("Resultado: " + result);
-		System.out.println("Resultado: " + aux);
+		//Digite o segundo cateto
+		double cat2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o cat2:"));
+		
+		//Digite o terceiro cateto
+		double cat3 = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o cat3:"));
+		
+		//Cálculo da Hipotenusa
+		double hypot = Math.hypot(cat1, cat2);
+		
+		System.out.println("Hypot: " + hypot);
+		
+		//O maior valor entre os catetos
+		double maximo = Math.max(cat1, Math.max(cat2, cat3));
+		System.out.println(maximo);
+		
+		for (int i = 0; i < 10; i++) 
+		{
+			double random = (int) (Math.random() * 10);
+			System.out.println("Número aleatório: " + i + " = " + random);
+		}
+		
 	}
 }
--- trunk/Linguagens/Java/UsandoClassesJava/src/br/com/devmedia/javaoo/UsandoModeloOOData.java
+++ trunk/Linguagens/Java/UsandoClassesJava/src/br/com/devmedia/javaoo/UsandoModeloOOData.java
@@ -1,44 +1,56 @@
 package br.com.devmedia.javaoo;
 
+import java.io.ObjectInputStream.GetField;
 import java.util.Date;
 
 import javax.swing.JOptionPane;
 
 public class UsandoModeloOOData 
 {
-	public static void main(String[] args) 
+	public static String getDataExtenso(String data) 
 	{
-		//entrada
-		
-		String data = JOptionPane.showInputDialog(null, "Digite uma data (dd/mm/aaaa)");
 		String meses[] = {"janeiro", "fevereiro", "março", "abril", "maio", "junho", 
-						  "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};
-		
+				  "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};
+
 		String diasdaSemana[] = {"Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", 
-									"Quinta-Feira", "Sexta-Feira", "Sábado"};
-		
+										"Quinta-Feira", "Sexta-Feira", "Sábado"};
+			
 		//processamento
-		
+			
 		String parte[] = data.split("/");
 		int dia = Integer.parseInt(parte[0]);
 		int mes = Integer.parseInt(parte[1]);
 		int ano = Integer.parseInt(parte[2]);
-		
+			
 		Date date = new Date();
 		date.setDate(dia);
 		date.setMonth((mes - 1));
 		date.setYear((ano - 1900));
-		
+			
 		int diaDaSemana = date.getDay();
-		
+			
 		String diaSemanaExtenso = diasdaSemana[diaDaSemana];
-		
+			
 		String mesExtenso = meses[mes - 1];
+			
+		//saída
+		return (diaSemanaExtenso + ", " + parte[0] + " de " + mesExtenso + " de " + parte[2]); 	
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+		//entrada
+		
+		String data = JOptionPane.showInputDialog(null, "Digite uma data (dd/mm/aaaa)");
+		
+		//processamento
+		
+		String dataExtenso = getDataExtenso(data);
 		
 		//saída
-
-		JOptionPane.showMessageDialog(null, diaSemanaExtenso + ", " + parte[0] + " de " + mesExtenso + " de " + parte[2]);
-		
+		JOptionPane.showMessageDialog(null, dataExtenso);
 	}
 
 }
