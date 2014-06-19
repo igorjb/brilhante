a614b4d53a17cfaff6b8b67cbc6af0218f80c2b6
igorjosebrilhante@gmail.com <igorjosebrilhante@gmail.com@9c8eae87-dc67-e4f6-7fff-6e8f81a4563f>
2014-06-19 21:41:08 +0000

    Cálculo das figuras planas e sólidas.

--- trunk/Linguagens/Java/UsandoNossasClasses/src/plana/Quadrilatero.java
+++ trunk/Linguagens/Java/UsandoNossasClasses/src/plana/Quadrilatero.java
@@ -5,20 +5,37 @@ public class Quadrilatero
 	private int lado1;
 	private int lado2;
 	
+	//Construtores
+	
+	//Sem parâmetros
+	public Quadrilatero()
+	{
+		this.lado1 = 10;
+		this.lado2 = 10;
+	}
+	
+	//Com parâmetros
+	public Quadrilatero(int l1, int l2)
+	{
+		this.lado1 = l1;
+		this.lado2 = l2;
+	}
+	
+	
 	public int getLado1() {
-		return lado1;
+		return this.lado1;
 	}
 	public int getLado2() {
-		return lado2;
+		return this.lado2;
 	}
 	
 	public void setLado1(int l1) 
 	{
 		if (l1 > 0)
 		{
-			lado1 = l1;
+			this.lado1 = l1;
 		} else {
-			lado1 = 0;
+			this.lado1 = 0;
 		}
 		
 	}
@@ -26,11 +43,48 @@ public class Quadrilatero
 	{
 		if (l2 > 0)
 		{
-			lado2 = l2;
+			this.lado2 = l2;
 		} else {
-			lado2 = 0;
+			this.lado2 = 0;
 		}
 		
+	}
+	
+	public String getDadosFormatados()
+	{
+		return this.lado1 + " - " + this.lado2;
+	}
+	
+	public String toString() 
+	{
+		return this.getDadosFormatados();
+	}
+	
+	public double getAreaPlana() 
+	{
+		return this.lado1 * this.lado2;
+	}
+	
+	public double getPerimetroPlana() 
+	{
+		return 2 * this.lado1 + 2 * this.lado2;
+	}
+	
+	public boolean isQuadradoPlana() 
+	{
+		if (this.lado1 == this.lado2)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public boolean isRetanguloPlana() 
+	{
+		return ! this.isQuadradoPlana();
 	}
 
 }
