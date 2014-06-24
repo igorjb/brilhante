56449fa6eae356620ceda9fd3f500e81190fab0f
igorjosebrilhante <igorjosebrilhante@gmail.com>
2014-06-24 22:52:07 +0000

    CRUD completo de Caloteiro utilizando Prepare Statement.

--- Cursos/TriadWorks/JavaParaWeb/ProjetoJDBC/src/br/com/triadworks/jdbc/main/SistemaCaloteiro.java
+++ Cursos/TriadWorks/JavaParaWeb/ProjetoJDBC/src/br/com/triadworks/jdbc/main/SistemaCaloteiro.java
@@ -1,6 +1,7 @@
 package br.com.triadworks.jdbc.main;
 
 import java.util.Calendar;
+import java.util.List;
 
 import br.com.triadworks.jdbc.dao.CaloteiroDAO;
 import br.com.triadworks.jdbc.modelo.Caloteiro;
@@ -9,6 +10,7 @@ public class SistemaCaloteiro {
 
 	public static void main(String[] args) 
 	{
+		//Inserir
 		Caloteiro caloteiro = new Caloteiro();
 		caloteiro.setNome("Alisson");
 		caloteiro.setEmail("alisson@caloteiro.com.br");
@@ -19,6 +21,37 @@ public class SistemaCaloteiro {
 		dao.adiciona(caloteiro);
 		
 		System.out.println("Gravado!");
+		
+		//Alterar
+		caloteiro.setNome("Joao");
+		caloteiro.setEmail("joao@caloteiro.com.br");
+		caloteiro.setDevendo(200);
+		caloteiro.setDataDivida(Calendar.getInstance());
+		caloteiro.setId(2l);
+		
+		
+		dao.altera(caloteiro);
+		System.out.println("Alterado!");
+		
+		//Excluir Registro Alterado
+		dao.deleta(caloteiro);
+		
+		System.out.println("Excluido!");
+		
+		//Listar
+		//acessando o getLista da DAO
+		
+		List<Caloteiro> lista = dao.getLista();
+		
+		for (Caloteiro c : lista) 
+		{
+			System.out.println("Nome: " + c.getNome());
+			System.out.println("Email: " + c.getEmail());
+			System.out.println("Devendo: R$ " + c.getDevendo() );
+			System.out.println("Data: " + c.getDataDivida().getTime() + "\n");
+		}
+		
+		
 	}
 
 }
