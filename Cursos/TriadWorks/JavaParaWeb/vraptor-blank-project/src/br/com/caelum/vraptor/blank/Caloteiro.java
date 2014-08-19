8c91818f22bd15726bb5aab470a358fe04723c9f
igorjosebrilhante <igorjosebrilhante@gmail.com>
2014-08-19 23:07:23 +0000

    Criei o model e adicionei as bibliotecas do servlet.

--- Cursos/TriadWorks/JavaParaWeb/vraptor-blank-project/src/br/com/caelum/vraptor/blank/Caloteiro.java
+++ Cursos/TriadWorks/JavaParaWeb/vraptor-blank-project/src/br/com/caelum/vraptor/blank/Caloteiro.java
@@ -1,5 +1,38 @@
 package br.com.caelum.vraptor.blank;
 
+import java.util.Calendar;
+
 public class Caloteiro {
 
+	private String nome;
+	private String email;
+	private Float devendo;
+	private Calendar dataDivida;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Float getDevendo() {
+		return devendo;
+	}
+	public void setDevendo(Float devendo) {
+		this.devendo = devendo;
+	}
+	public Calendar getDataDivida() {
+		return dataDivida;
+	}
+	public void setDataDivida(Calendar dataDivida) {
+		this.dataDivida = dataDivida;
+	}
+	
+	
 }
