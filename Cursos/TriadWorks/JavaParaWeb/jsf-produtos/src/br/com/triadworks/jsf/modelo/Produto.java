309f0d88703a616f22ffddd072f358d8ba2b0f4f
igorjosebrilhante <igorjosebrilhante@gmail.com>
2014-08-20 23:10:57 +0000

    cadastro básico de produtos.

---
+++ Cursos/TriadWorks/JavaParaWeb/jsf-produtos/src/br/com/triadworks/jsf/modelo/Produto.java
@@ -0,0 +1,29 @@
+package br.com.triadworks.jsf.modelo;
+
+public class Produto {
+
+	private String nome;
+	private String descricao;
+	private Double preco;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	
+}
