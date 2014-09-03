2c02feb2a1c82352db271dbec514fd30b03ee1cf
igorjosebrilhante@gmail.com <igorjosebrilhante@gmail.com@9c8eae87-dc67-e4f6-7fff-6e8f81a4563f>
2014-09-03 03:20:44 +0000

    Initial import.

---
+++ trunk/Cursos/TriadWorks/JavaParaWeb/jpa-produtos/src/br/com/triadworks/produtos/modelo/Produto.java
@@ -0,0 +1,44 @@
+package br.com.triadworks.produtos.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String nome;
+	private String descricao;
+	private double preco;
+	
+	public Integer getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+}
