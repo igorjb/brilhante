70caa12fa45287a865a63be3e2bdd9e4d0533c1e
igorjosebrilhante <igorjosebrilhante@gmail.com>
2014-09-09 02:57:29 +0000

    Navegação entre páginas.

--- Linguagens/Java/JSF/PrimeiraAppComJSF2/src/controle/ControlePrincipal.java
+++ Linguagens/Java/JSF/PrimeiraAppComJSF2/src/controle/ControlePrincipal.java
@@ -6,10 +6,10 @@ import java.util.Calendar;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 //import javax.faces.bean.RequestScoped;
-//import javax.faces.bean.SessionScoped;
+import javax.faces.bean.SessionScoped;
 
 @ManagedBean(name = "controlePrincipal")
-@ApplicationScoped
+@SessionScoped
 public class ControlePrincipal {
 
 	public String mensagem;
@@ -20,6 +20,27 @@ public class ControlePrincipal {
 		mensagem = "A aplicação foi ao ar em: " + sdf.format(Calendar.getInstance().getTime());
 	}
 
+	public String sobre() {
+		mensagem = "Você navegou de maneira dinâmica";
+		return "sobre";
+	}
+	
+	public String sobreRedirecionado() {
+		mensagem = "Voce foi redirecionado para a pagina sobre.xhtml";
+		return "sobre?faces-redirect=true";
+		
+	}
+	
+	public String geraErro() {
+		return "erro";
+	}
+	
+	public String geraErroGrave() {
+		return "erro";
+	}
+	public String geraLogin() {
+		return "login";
+	}
 	public String getMensagem() {
 		return mensagem;
 	}
