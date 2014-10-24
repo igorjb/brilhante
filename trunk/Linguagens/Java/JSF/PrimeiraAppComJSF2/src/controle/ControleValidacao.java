3abe99b7da7e299ff4e8c077e6da476b35e348cd
igorjosebrilhante@gmail.com <igorjosebrilhante@gmail.com@9c8eae87-dc67-e4f6-7fff-6e8f81a4563f>
2014-10-24 02:23:14 +0000

    Validando Valores com a Bean Validation Api.

--- trunk/Linguagens/Java/JSF/PrimeiraAppComJSF2/src/controle/ControleValidacao.java
+++ trunk/Linguagens/Java/JSF/PrimeiraAppComJSF2/src/controle/ControleValidacao.java
@@ -6,14 +6,36 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Range;
 
 @ManagedBean(name="controleValidacao")
 @RequestScoped
 public class ControleValidacao implements Serializable {
+	
+	@NotEmpty(message="O nome deve ser informado")
+	@Length(max=40, min=3, message="O nome deve estar entre {min} e {max} caracteres")
 	private String nome;
+	
+	@NotEmpty(message="O telefone deve ser informado")
+	@Pattern(regexp="\\(\\d{2}\\)\\d{4}\\-\\d{4}", message="O formato do telefone deve ser (99)9999-9999")
 	private String telefone;
+	
+	@Email(message="Informe um email valido")
+	@NotEmpty(message="O email deve ser informado")
 	private String email;
+	
+	@NotNull(message="A idade deve ser informada")
+	@Range(min=12, max=80, message="A idade deve estar entre {min} e {max} anos")
 	private Long idade;
+	
+	@NotNull(message="A nota deve ser informada")
+	@Range(min=0, max=10, message="A nota deve estar entre {min} e {max}")
 	private Double nota;
 	
 	public ControleValidacao()
