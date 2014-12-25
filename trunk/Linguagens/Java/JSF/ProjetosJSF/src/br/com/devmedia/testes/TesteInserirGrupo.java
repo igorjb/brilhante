369ed5e186e29b48433f3940680ae0e27a087cae
igorjosebrilhante@gmail.com <igorjosebrilhante@gmail.com@9c8eae87-dc67-e4f6-7fff-6e8f81a4563f>
2014-12-25 19:18:47 +0000

    Configuração da JPA e criação de classes persistentes e banco de dados.

---
+++ trunk/Linguagens/Java/JSF/ProjetosJSF/src/br/com/devmedia/testes/TesteInserirGrupo.java
@@ -0,0 +1,18 @@
+package br.com.devmedia.testes;
+
+import javax.persistence.EntityManager;
+
+import br.com.devmedia.beans.Grupo;
+import br.com.devmedia.jpa.EntityManagerUtil;
+
+public class TesteInserirGrupo {
+
+	public static void main(String[] args) {
+		EntityManager em = EntityManagerUtil.getEntityManager();
+		Grupo g = new Grupo();
+		g.setNome("Gestores");
+		em.getTransaction().begin();
+		em.persist(g);
+		em.getTransaction().commit();
+	}
+}
