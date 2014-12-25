afebed5725fbae3608c4674fd71246ab491bef95
igorjosebrilhante <igorjosebrilhante@gmail.com>
2014-12-25 19:18:47 +0000

    Configuração da JPA e criação de classes persistentes e banco de dados.

---
+++ Linguagens/Java/JSF/ProjetosJSF/src/br/com/devmedia/jpa/EntityManagerUtil.java
@@ -0,0 +1,24 @@
+package br.com.devmedia.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManagerUtil {
+
+	private static EntityManagerFactory factory = null;
+	private static EntityManager em = null;
+	
+	public static EntityManager getEntityManager() {
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory("ProjetosJSF");
+		}
+		
+		if (em == null) {
+			em = factory.createEntityManager();
+		}
+		
+		return em;
+	}
+
+}
