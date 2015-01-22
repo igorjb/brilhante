bf7d259e0d70d0a72de2816cc7034bc005aa3634
igorjosebrilhante@gmail.com <igorjosebrilhante@gmail.com@9c8eae87-dc67-e4f6-7fff-6e8f81a4563f>
2015-01-22 03:12:53 +0000

    DAO Generico para acesso aos dados - segunda e ultima parte.

---
+++ trunk/Linguagens/Java/JSF/ProjetosJSF/src/br/com/devmedia/modelo/DAOSetor.java
@@ -0,0 +1,23 @@
+package br.com.devmedia.modelo;
+
+import java.io.Serializable;
+
+import br.com.devmedia.beans.Setor;
+import br.com.devmedia.conversores.ConverterOrdem;
+import br.com.devmedia.jpa.EntityManagerUtil;
+
+@SuppressWarnings("serial")
+public class DAOSetor<T> extends GenericDAO<T> implements Serializable {
+
+	public DAOSetor() {
+		super.setClasse(Setor.class);
+		super.setEm(EntityManagerUtil.getEntityManager());
+		super.getListaOrdem().add(new Ordem("Código", "id"));
+		super.getListaOrdem().add(new Ordem("Nome", "nome"));
+		super.setOrdemAtual((Ordem) super.getListaOrdem().get(1));
+		super.setFiltro("");
+		super.setMaximoObjetos(2);
+		super.setConverterOrdem(new ConverterOrdem(super.getListaOrdem()));
+	}
+
+}
