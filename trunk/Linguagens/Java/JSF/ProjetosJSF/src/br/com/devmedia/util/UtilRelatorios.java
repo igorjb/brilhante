e7d79d5e75a66a7788de17f2621af0340f92a38a
igorjosebrilhante@gmail.com <igorjosebrilhante@gmail.com@9c8eae87-dc67-e4f6-7fff-6e8f81a4563f>
2015-02-20 03:11:10 +0000

    Classe para imprimir Relatório e relatório de funcionários.

---
+++ trunk/Linguagens/Java/JSF/ProjetosJSF/src/br/com/devmedia/util/UtilRelatorios.java
@@ -0,0 +1,39 @@
+package br.com.devmedia.util;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+public class UtilRelatorios {
+	public static void imprimeRelatorio(String relatorioNome,
+			HashMap parametros, List lista) {
+		try {
+			JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(lista);
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			ServletContext scontext = (ServletContext) facesContext.getExternalContext().getContext();
+			String path = scontext.getRealPath("/WEB-INF/relatorios/");
+			parametros.put("SUBREPORT_DIR", path + File.separator);
+			JasperPrint jasperPrint = JasperFillManager.fillReport(path + relatorioNome+".jasper", parametros, dataSource);
+			byte[] b = JasperExportManager.exportReportToPdf(jasperPrint);
+			HttpServletResponse res = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+			res.setContentType("application/pdf");
+			int codigo = (int) (Math.random() * 1000);
+			res.setHeader("Content-disposition", "inline);filename=relatorio_"+codigo+".pdf");
+			res.getOutputStream().write(b);
+			facesContext.renderResponse();
+		} catch (Exception e) {
+			// TODO: handle exception
+			UtilMensagens.mensagemErro("Erro ao imprimir: "+UtilErros.getMenssagemErro(e));
+			e.printStackTrace();
+		}
+	}
+}
