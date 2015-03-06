db3d153a86ddaecb39abf8a6e3c93b4b7539b4ad
igorjosebrilhante@gmail.com <igorjosebrilhante@gmail.com@9c8eae87-dc67-e4f6-7fff-6e8f81a4563f>
2015-03-06 02:59:08 +0000

    Gerador de relatorios.

---
+++ trunk/Linguagens/Java/JSF/ProjetosJSF/src/br/com/devmedia/testes/RelatorioDeFuncionarios.java
@@ -0,0 +1,62 @@
+package br.com.devmedia.testes;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.devmedia.beans.Funcionario;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+public class RelatorioDeFuncionarios {
+	public static void main(String[] args) throws JRException, SQLException {
+
+		System.out.println("Gerando relatório...");
+		// lista com os nossos clientes
+		List lista = new ArrayList();
+
+		Funcionario c1 = new Funcionario();
+		c1.setNome("Alexandre Macedo");
+		c1.setEmail("alexbmac@gmail.com");
+
+		Funcionario c2 = new Funcionario();
+		c2.setNome("Rafael Cosentino");
+		c2.setEmail("cosen@gmail.com");
+
+		Funcionario c3 = new Funcionario();
+		c3.setNome("Daniel Machado");
+		c3.setEmail("daniel@gmail.com");
+
+		lista.add(c1);
+		lista.add(c2);
+		lista.add(c3);
+
+		// compilacao do JRXML
+		JasperReport report = JasperCompileManager
+				.compileReport("C:/Users/Win7/workspace/ProjetosJSF/WebContent/WEB-INF/relatorios/funcionarios.jrxml");
+
+		// preenchimento do relatorio, note que o metodo recebe 3 parametros:
+		// 1 - o relatorio
+		//
+		// 2 - um Map, com parametros que sao passados ao relatorio
+		// no momento do preenchimento. No nosso caso eh null, pois nao
+		// estamos usando nenhum parametro
+		//
+		// 3 - o data source. Note que nao devemos passar a lista diretamente,
+		// e sim "transformar" em um data source utilizando a classe
+		// JRBeanCollectionDataSource
+		JasperPrint print = JasperFillManager.fillReport(report, null,
+				new JRBeanCollectionDataSource(lista));
+
+		// exportacao do relatorio para outro formato, no caso PDF
+		JasperExportManager.exportReportToPdfFile(print,
+				"C:/Users/Win7/workspace/ProjetosJSF/WebContent/WEB-INF/relatorios/RelatorioFuncionarios.pdf");
+
+		System.out.println("Relatório gerado.");
+	}
+}
