01792d5a33d2b1bebf5e809a100ea4d7b21e8727
igorjosebrilhante@gmail.com <igorjosebrilhante@gmail.com@9c8eae87-dc67-e4f6-7fff-6e8f81a4563f>
2015-05-14 03:02:56 +0000

    Adicionando filtro, ordenação e paginação no DAO de projetos (primeira parte).

--- trunk/Linguagens/Java/JSF/ProjetosJSF/src/br/com/devmedia/modelo/ProjetoDAO.java
+++ trunk/Linguagens/Java/JSF/ProjetosJSF/src/br/com/devmedia/modelo/ProjetoDAO.java
@@ -12,7 +12,11 @@ import br.com.devmedia.util.UtilMensagens;
 public class ProjetoDAO {
 
 	private EntityManager em;
-	
+	private String ordem = "id";
+	private String filtro = "";
+	private Integer maximosObjetos = 2;
+	private Integer posicaoAtual = 0;
+	private Integer totalObjetos = 0;
 	
 	public EntityManager getEm() {
 		return em;
@@ -26,6 +30,66 @@ public class ProjetoDAO {
 		em = EntityManagerUtil.getEntityManager();
 	}
 	
+	@SuppressWarnings("unchecked")
+	public List<Projeto> listar() {
+		String where = "";
+		if (filtro.length() > 0)
+		{
+			if (ordem.equals("id"))
+			{
+				try {
+					Integer.parseInt(filtro);
+					where = " where " + ordem + " = '" + filtro + "' ";
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+			} else {
+				where = " where upper("+ordem+") like '"+filtro.toUpperCase()+ "%' ";
+			}
+		}
+		String jpql = "from Projeto " + where + " order by " + ordem;
+		totalObjetos = em.createQuery("select id from Projeto " + where + " order by "+ ordem).getResultList().size();
+		
+		return em.createQuery(jpql).setFirstResult(posicaoAtual).setMaxResults(maximosObjetos).getResultList();
+	}
+	
+	public void primeiro() {
+		posicaoAtual = 0;
+	}
+	
+	public void anterior() {
+		posicaoAtual -= maximosObjetos;
+		if (posicaoAtual < 0){
+			posicaoAtual = 0;
+		}
+	}
+	
+	public void proximo() {
+		if (posicaoAtual + maximosObjetos < totalObjetos){
+			posicaoAtual += maximosObjetos;
+		}
+	}
+	
+	public void ultimo() {
+		int resto = totalObjetos % maximosObjetos;
+		
+		if (resto > 0){
+			posicaoAtual = totalObjetos - resto;
+		}else{
+			posicaoAtual = totalObjetos - maximosObjetos;
+		}
+	}
+	
+	public String getMensagemNavegacao() {
+		int ate = posicaoAtual + maximosObjetos;
+		if (ate > totalObjetos){
+			ate = totalObjetos;
+		}
+		
+		return "Listando de " + (posicaoAtual + 1) + 
+				"até " + ate + " de " + totalObjetos + " registros";
+	}
+	@SuppressWarnings("unchecked")
 	public List<Projeto> listarTodos() {
 		return em.createQuery("from Projeto order by nome").getResultList();
 	}
@@ -78,4 +142,46 @@ public class ProjetoDAO {
 		}
 		em.getTransaction().rollback();
 	}
+
+	public String getOrdem() {
+		return ordem;
+	}
+
+	public void setOrdem(String ordem) {
+		this.ordem = ordem;
+	}
+
+	public String getFiltro() {
+		return filtro;
+	}
+
+	public void setFiltro(String filtro) {
+		this.filtro = filtro;
+	}
+
+	public Integer getMaximosObjetos() {
+		return maximosObjetos;
+	}
+
+	public void setMaximosObjetos(Integer maximosObjetos) {
+		this.maximosObjetos = maximosObjetos;
+	}
+
+	public Integer getPosicaoAtual() {
+		return posicaoAtual;
+	}
+
+	public void setPosicaoAtual(Integer posicaoAtual) {
+		this.posicaoAtual = posicaoAtual;
+	}
+
+	public Integer getTotalObjetos() {
+		return totalObjetos;
+	}
+
+	public void setTotalObjetos(Integer totalObjetos) {
+		this.totalObjetos = totalObjetos;
+	}
+	
+	
 }
